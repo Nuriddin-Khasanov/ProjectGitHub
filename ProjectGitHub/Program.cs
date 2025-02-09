99c19c1aa031a1d896f4fdca8285d96f12efd7e4
Nuriddin-Khasanov <nuriddinhasanov44@gmail.com>
2025-02-09 22:53:04 +0500

    Добавьте файлы проекта.

---
+++ ProjectGitHub/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine("Sum of a and b is: " + c);
+            Console.ReadLine();
+
+        }
+    }
+}
